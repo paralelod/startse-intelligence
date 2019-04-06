b316c024b69f09ca5ded1ba68f55dbf4e29cc88c
diogorusso <hello.diogorusso@gmail.com>
2019-04-06 11:52:30 -0700

    Updates

---
+++ precache-manifest.5de962e437a92b58eb6bccda70ab42af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "efba5ad4738d1aa6663f7384a6a3007b",
+    "url": "/startse-intelligence/static/media/logo.efba5ad4.svg"
+  },
+  {
+    "revision": "e37e507951956d1f1aa2",
+    "url": "/startse-intelligence/static/js/runtime~main.88d0dc13.js"
+  },
+  {
+    "revision": "977b6921171fe2ab31d0",
+    "url": "/startse-intelligence/static/js/main.ea857024.chunk.js"
+  },
+  {
+    "revision": "9fb58431426fcb292e46",
+    "url": "/startse-intelligence/static/js/2.088dc6fc.chunk.js"
+  },
+  {
+    "revision": "977b6921171fe2ab31d0",
+    "url": "/startse-intelligence/static/css/main.83304017.chunk.css"
+  },
+  {
+    "revision": "9fb58431426fcb292e46",
+    "url": "/startse-intelligence/static/css/2.d962b8be.chunk.css"
+  },
+  {
+    "revision": "c8a84b6c7526b6832faa6a6529e4b3af",
+    "url": "/startse-intelligence/index.html"
+  }
+];
